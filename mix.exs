a158dd7fffa3af4d4724ac3e18ee92bb6c64c2ee
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2022-02-01 12:17:39 +0300

    [DT-809]: extend :riak dependency definition with outdated-warning-comment

--- mix.exs
+++ mix.exs
@@ -19,6 +19,9 @@ defmodule Konvex.MixProject do
 
   defp deps do
     [
+      # :riak is outdated wrapper for :riak-erlang-client
+      # (furthermore it utilizes incompatible with OTP 24 :pooler)
+      # TODO: Use https://github.com/basho/riak-erlang-client directly instead
       {:riak, "~> 1.1.6"}
     ]
   end
