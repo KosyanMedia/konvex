3c3b4843ea8cd333a4cf7adceb3a2f9fe2abd56f
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2022-02-07 15:34:52 +0300

    [DT-809]: remove Konvex.Implementation.Riak.Connection.Provider-related redundant modules

--- lib/konvex/implementation/riak/connection/provider/pid.ex
+++
@@ -1,7 +0,0 @@
-defimpl Konvex.Implementation.Riak.Connection.Provider, for: PID do
-  @moduledoc """
-  So you can feel free to pass connection PID itself as a connection provider
-  """
-
-  def get_connection_pid(pid), do: pid
-end
