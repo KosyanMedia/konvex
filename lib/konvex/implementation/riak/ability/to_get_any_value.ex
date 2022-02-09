fe347d7ce90f67ae47783aa5b893c319a8c0bea9
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2022-02-09 13:09:51 +0300

    [DT-809]: introduce Konvex.Implementation.Riak.Ability.ToGetAnyValue

---
+++ lib/konvex/implementation/riak/ability/to_get_any_value.ex
@@ -0,0 +1,31 @@
+defmodule Konvex.Implementation.Riak.Ability.ToGetAnyValue do
+  defmacro __using__(
+             [
+               bucket_name: <<_, _ :: binary>> = bucket_name,
+               conflict_resolution_strategy_module: conflict_resolution_strategy_module,
+               connection: quoted_riak_connection
+             ]
+           ) do
+    quote do
+      alias Konvex.Implementation.Riak.Connection
+
+      defmodule Private.Implementation.Ability.ToGetTextValue do
+        use Konvex.Implementation.Riak.Ability.ToGetTextValue,
+            bucket_name: unquote(bucket_name),
+            conflict_resolution_strategy_module: unquote(conflict_resolution_strategy_module),
+            connection: unquote(quoted_riak_connection)
+      end
+
+      @behaviour Konvex.Ability.ToGetAnyValue
+
+      @impl Konvex.Ability.ToGetAnyValue
+      @spec get_any_value(key :: String.t) :: :key_not_found | any
+      def get_any_value(key) when is_binary(key) do
+        with binary_value when is_binary(binary_value)
+             <- Private.Implementation.Ability.ToGetTextValue.get_text_value(key) do
+          :erlang.binary_to_term(binary_value)
+        end
+      end
+    end
+  end
+end
