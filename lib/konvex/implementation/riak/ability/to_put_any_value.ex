6feccb0b22670d021367b328022ba5525a05c55b
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2022-02-09 13:09:59 +0300

    [DT-809]: introduce Konvex.Implementation.Riak.Ability.ToPutAnyValue

---
+++ lib/konvex/implementation/riak/ability/to_put_any_value.ex
@@ -0,0 +1,26 @@
+defmodule Konvex.Implementation.Riak.Ability.ToPutAnyValue do
+  defmacro __using__(
+             [
+               bucket_name: <<_, _ :: binary>> = bucket_name,
+               connection: quoted_riak_connection
+             ]
+           ) do
+    quote do
+      alias Konvex.Implementation.Riak.Connection
+
+      defmodule Private.Implementation.Ability.ToPutTextValue do
+        use Konvex.Implementation.Riak.Ability.ToPutTextValue,
+            bucket_name: unquote(bucket_name),
+            connection: unquote(quoted_riak_connection)
+      end
+
+      @behaviour Konvex.Ability.ToPutAnyValue
+
+      @impl Konvex.Ability.ToPutAnyValue
+      @spec put_any_value(key :: String.t, value :: any) :: :unit
+      def put_any_value(key, value) when is_binary(key) do
+        Private.Implementation.Ability.ToPutTextValue.put_text_value(key, :erlang.term_to_binary(value))
+      end
+    end
+  end
+end
