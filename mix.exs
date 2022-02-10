a78fbce0a6962586f527bd836a166af597ae1c49
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2022-02-10 12:26:57 +0300

    [DT-809]: update library version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Konvex.MixProject do
   def project do
     [
       app: :konvex,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
