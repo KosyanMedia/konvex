a8004cc39d58e2889da7fa619079f82dacf8bdf4
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2023-05-15 20:18:07 +0800

    Relax project's Elixir version requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Konvex.MixProject do
     [
       app: :konvex,
       version: "1.1.6",
-      elixir: "~> 1.13",
+      elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
