32bc7e8c92f447dc9b3e51d2f3102ea1700476fc
Mikhail Kostin <me.kostin@gmail.com>
2023-05-17 13:06:24 +0700

    riak_pb custom compiling method

--- mix.exs
+++ mix.exs
@@ -20,7 +20,15 @@ defmodule Konvex.MixProject do
   defp deps do
     [
       # FIXME: This is a workaround to force skip :protobuffs transitive dependency
-      {:riak_pb, github: "basho/riak_pb", ref: "46d5f4d1b899fbde4357f9d777a8c71d8ac66a35", override: true},
+      {
+        :riak_pb,
+        github: "basho/riak_pb",
+        ref: "46d5f4d1b899fbde4357f9d777a8c71d8ac66a35",
+        override: true,
+        # NOTE: This is a workaround for building riak_pb with Elixir >= 1.13
+        compile:
+          "#{Mix.Rebar.local_rebar_path(:rebar3)} bare compile --paths #{Mix.Project.build_path()}/lib/*/ebin"
+      },
       {:riakc, github: "basho/riak-erlang-client", tag: "3.0.8+p1"}
     ]
   end
