a8b74b33845074b9f75534acbfc6ad1c9f90df29
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2023-05-20 16:34:01 +0800

    Refactor connection usage facility to utilize Resource

--- lib/konvex/implementation/riak/connection/usage.ex
+++ lib/konvex/implementation/riak/connection/usage.ex
@@ -6,13 +6,10 @@ defmodule Konvex.Implementation.Riak.Connection.Usage do
           connection_resource_usage :: ((opened_connection_pid :: pid) -> (usage_result :: any))
         ) :: (usage_result :: any)
   def using(connection_resource, connection_resource_usage) do
-    with opened_connection_pid when is_pid(opened_connection_pid)
-         <- Connection.open(connection_resource) do
-      try do
-        connection_resource_usage.(opened_connection_pid)
-      after
-        Connection.close(connection_resource, opened_connection_pid)
-      end
-    end
+    Resource.create(
+      acquire: fn -> Connection.open(connection_resource) end,
+      release: &Connection.close(connection_resource, &1)
+    )
+    |> Resource.use!(&connection_resource_usage.(&1))
   end
 end
